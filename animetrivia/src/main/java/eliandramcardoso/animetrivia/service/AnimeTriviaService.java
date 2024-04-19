199e86b1af31f016dc2a6a53a87bb816fd60e012
ardnaile <elimottacardoso@gmail.com>
2024-04-19 16:52:46 -0300

    Debbugando

--- animetrivia/src/main/java/eliandramcardoso/animetrivia/service/AnimeTriviaService.java
+++ animetrivia/src/main/java/eliandramcardoso/animetrivia/service/AnimeTriviaService.java
@@ -1,6 +1,6 @@
 package eliandramcardoso.animetrivia.service;
 
-import eliandramcardoso.animetrivia.models.AnimeInfo;
+import eliandramcardoso.animetrivia.models.AnimeInfoApi;
 import eliandramcardoso.animetrivia.models.respostaUser;
 import org.json.JSONObject;
 import org.springframework.stereotype.Service;
@@ -10,34 +10,25 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 public class AnimeTriviaService {
-    private static final AtomicInteger idCounter = new AtomicInteger(1);
-
-    public static String[] getArrayInfoAnime(){
-        String[] infoAnime = new String[4];
-
-        infoAnime[0] = AnimeInfo.getQuote();
-        infoAnime[1] = AnimeInfo.getAnime();
-        infoAnime[2] = AnimeInfo.getCharacter();
-        infoAnime[3] = String.valueOf(idCounter.getAndIncrement());
-
-        return infoAnime;
+    public String[] getArray(){
+        String[] array = new String[4];
+        array[0] = AnimeInfoApi.getQuote();
+        array[1] = AnimeInfoApi.getAnime();
+        array[2] = AnimeInfoApi.getCharacter();
+        array[3] = AnimeInfoApi.getId();
+        return array;
     }
-    public String frase() {
-        String[] array = getArrayInfoAnime();
-        String frase = array[0];
-        String anime = array[1];
-        String id = array[3];
-        return ("A frase \"" + frase + "\", do anime " + anime +
-                " foi dita por qual personagem? Responda na requisição /validar com o id " + id);
+    public String frase(){
+        String[] array = getArray();
+        System.out.println("id em frase: " + array[3]);
+        return ("A frase \"" + array[0] + "\", do anime " + array[1] +
+                " foi dita por qual personagem? Responda na requisição /validar com o id " + array[3]);
     }
-
     public String validar(respostaUser resposta){
-        String[] array = getArrayInfoAnime();
-        String personagem = array[2];
-        String id = array[3];
-
-        boolean acertou = (Objects.equals(resposta.getIdUser(), id) && Objects.equals(resposta.getPersonagemUser(), personagem));
-        boolean idErrado = (!Objects.equals(resposta.getIdUser(), id));
+        String[] array = getArray();
+        System.out.println("id em validar: " + array[3]);
+        boolean acertou = (Objects.equals(resposta.getIdUser(), array[3]) && Objects.equals(resposta.getPersonagemUser(), array[2]));
+        boolean idErrado = (!Objects.equals(resposta.getIdUser(), array[3]));
 
         if (acertou){
             return ("Parabéns acertou");
