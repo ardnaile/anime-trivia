41052d058a14db3935aa7c80c14cedc0a6fcc854
ardnaile <elimottacardoso@gmail.com>
2024-04-19 19:50:19 -0300

    Correções finais

--- animetrivia/src/main/java/eliandramcardoso/animetrivia/service/AnimeTriviaService.java
+++ animetrivia/src/main/java/eliandramcardoso/animetrivia/service/AnimeTriviaService.java
@@ -1,41 +1,53 @@
 package eliandramcardoso.animetrivia.service;
 
-import eliandramcardoso.animetrivia.models.AnimeInfoApi;
 import eliandramcardoso.animetrivia.models.respostaUser;
 import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 import java.util.Objects;
-import java.util.concurrent.atomic.AtomicInteger;
 
+import static eliandramcardoso.animetrivia.service.AnimeApiService.getArrayFromAPI;
 
 @Service
 public class AnimeTriviaService {
-    public String[] getArray(){
-        String[] array = new String[4];
-        array[0] = AnimeInfoApi.getQuote();
-        array[1] = AnimeInfoApi.getAnime();
-        array[2] = AnimeInfoApi.getCharacter();
-        array[3] = AnimeInfoApi.getId();
-        return array;
-    }
-    public String frase(){
-        String[] array = getArray();
-        System.out.println("id em frase: " + array[3]);
-        return ("A frase \"" + array[0] + "\", do anime " + array[1] +
-                " foi dita por qual personagem? Responda na requisição /validar com o id " + array[3]);
-    }
-    public String validar(respostaUser resposta){
-        String[] array = getArray();
-        System.out.println("id em validar: " + array[3]);
-        boolean acertou = (Objects.equals(resposta.getIdUser(), array[3]) && Objects.equals(resposta.getPersonagemUser(), array[2]));
-        boolean idErrado = (!Objects.equals(resposta.getIdUser(), array[3]));
+    private String[] lastCharacterData;
 
-        if (acertou){
-            return ("Parabéns acertou");
-        } else if (idErrado){
+    public String jogar(){
+        lastCharacterData = getArrayFromAPI();
+
+        String personagem = lastCharacterData[2];
+        String id = lastCharacterData[3];
+
+        System.out.println("id em frase: " + id); // debug
+
+        JSONObject jsonExemplo = new JSONObject();
+        jsonExemplo.put("resposta", "<insira aqui sua resposta>");
+        jsonExemplo.put("id", id);
+
+        return ("O personagem " + personagem + " está em qual anime? \n\nFaça uma requisição POST no caminho /jogar/responder com o id " +
+                id + " e a sua resposta em formato json! \n\nExemplo: " + jsonExemplo);
+    }
+    public String validar(respostaUser resposta) {
+        String anime = lastCharacterData[1];
+        String id = lastCharacterData[3];
+
+        System.out.println("id em validar: " + id); // debug
+
+        // tratando as strings antes de validar!
+        String idLowerCase = id.toLowerCase();
+        String animeLowerCase = anime.toLowerCase();
+        String respostaIdLowerCase = resposta.getId().toLowerCase();
+
+        boolean acertou = (respostaIdLowerCase.equals(idLowerCase) && resposta.getResposta().equalsIgnoreCase(animeLowerCase));
+        boolean idErrado = (!respostaIdLowerCase.equals(idLowerCase));
+
+        if (lastCharacterData == null) {
+            return ("Não há dados para validar. Faça uma requisição para /jogar primeiro.");
+        }else if (acertou){
+            return ("Boa! Você acertou!\n\nFaça uma requisição GET para /jogar para jogar novamente!");
+        }else if (idErrado){
             return ("O id que você forneceu não é o mesmo da frase que você viu!");
         }
-        return ("Parabéns errou!");
+        return ("Ops! Você errou!\n\nFaça uma requisição GET para /jogar para jogar novamente!");
     }
 
     public String obterJsonAjuda(){
